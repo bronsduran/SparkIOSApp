2f8cd50bf0d7e2b9db46756fd0b516e0ca7b05ef
Bronson Duran <brons@outlook.com>
2016-01-16 12:51:53 -0800

    Initial Commit

---
+++ Spark1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spark1.0
+//
+//  Created by Bronson Duran on 1/16/16.
+//  Copyright © 2016 Bronson Duran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
