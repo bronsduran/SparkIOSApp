d2f50c844e9da5f17a3c0fe82334955b133267cc
Kevin Khieu <kkhieu@stanford.edu>
2016-01-19 20:38:57 -0800

    Parse added

--- Spark1.0/AppDelegate.swift
+++ Spark1.0/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("4rGVNu0fYHu6DhBESJooVeMD3iYKelTNqtfMmJIW",
+            clientKey: "pb5VCMKxdFET5Kjn4xFEhvAj3WkLUpxjbT9ZfaFF")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         return true
     }
 
